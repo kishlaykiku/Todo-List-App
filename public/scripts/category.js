9b01dc53fe2b4c2c5314e2fb5d165fd207a85650
kishlay <kishlaykumarkkp24@gmail.com>
2023-08-10 16:33:36 +0530

    Doccumented and Refactored

--- public/scripts/category.js
+++ public/scripts/category.js
@@ -1,24 +1,38 @@
 // Global functions
 function camelCase(str) {
+
+    // Store the string and split them according to spaces as delimeter
     let arr = str.toLowerCase().split(" ");
+    // Initialize an empty temporary string storage variable
     let temp = "";
+
+    // Loop through the splited string array
     for(let i = 1; i < arr.length; i++)
     {
+        // Camel casing
         temp = temp + arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
     }
+    // Return the camel cased string
     return arr[0] + temp;
 }
 
 
-// ---------------------------------- Category section
-// Store the add-icon button
+// ---------------------------------- Category section ----------------------------------
+
+// When add-icon button is clicked, call this function to display temporary input field for new category
 function inputCategory() {
+
+    // Keep count of the number of categories already in the categories-panel
     let categoryCount = $('.categories-panel').children('div').length;
+    // Keep count of the number of temporary input field in the categories-panel
     let mainCount = $('.categories-panel').children('main').length;
 
+    // If the number of categories is less than = 4 and temporary input filed is not present in the categories-panel, validate the condition
     if(categoryCount != 4 && mainCount == 0) {
+        // First remove the add-icon button
         $("#category-add").remove();
         
+        // Create a new temporary input field which will hold the new category title
         let inputNewCategory = 
             "<main class='flex-row temp-input-box'>" + 
             "<div style='position: relative; width: 50%'>" + 
@@ -29,31 +43,37 @@ function inputCategory() {
             "<button class='category-cancel' id='category-submit' onclick='removeNew()'><i class='icon fa-solid fa-xmark'></i></button>" + 
             "</main>";
 
+        // Now append the temporary input field for new category in the categories-panel
         $('.categories-panel').append(inputNewCategory);
         $('.categories-panel .temp-input-box').animate({opacity: '1'});
     }
+    // If number of categories is more than 4, then remove the add-icon button for new category (Limit the no. of categories to 4)
     else {
         $('#category-add').remove();
     }
 }
 
+// Call when new category is to be pushed in the categories-panel
 function push() {
+    // Store the input string and replace all the special characters with empty character
     let categoryTitle = $('#categoryTitle').val().trim().toString().replace(/[^a-z0-9 \b]/gi, '');
 
+    // Camel Case the new category title
     let storeID = camelCase(categoryTitle);
 
+    // Loop to check if a category with the same ID is already present
     $('.categories-panel div input[type="radio"]').each(function () {
+        // If yes then add 1 at the end of the title to make it distinguishable
         if((storeID) == $(this).attr('id'))
         {
             storeID = storeID+1;
         }
     });
 
-    if(storeID == $('.categories-panel div input[type="radio"]').attr('id')) {
-        storeID = storeID+1;
-    }
-
+    // Validate the length of the new category title
     if(categoryTitle.length > 0 && categoryTitle.length < 13) {
+
+        // Create a new category that displays the new category title
         let newCategory = 
             `<div class='extra-category' id='${storeID}'>` + 
             `<input type='radio' id='${storeID}' name='category' value='${categoryTitle.split(" ").join("-").toLowerCase()}'></input>` + 
@@ -61,85 +81,134 @@ function push() {
             `<button class='remove-category' id='removeCategory' onclick=''><i class='icon fa-solid fa-xmark'></i></button>` + 
             `</div>`;
 
+        // Remove the temporary input box for new category
         $('.categories-panel .temp-input-box').remove();
+
+        // Append the new category in categories-panel
         $('.categories-panel').append(newCategory);
+
+        // Calling this function to create a divin task-panel section that will display the tasks under the new added category
         addTaskCategory();
+
+        // Make the new category selected by default
         makeNewActive(storeID);
 
+        // Initialize the add-icon button
         const addButton = "<button id='category-add' class='add-icon font' title='Add Category' onclick='inputCategory()'><i class='icon fa-solid fa-plus'></i></button>";
+        // Get the length of the number of categories in categories-panel
         let categoryCount = $('.categories-panel').children('div').length;
+        // Limit the number of categories to 4. Don't display the add-icon button if there are already 4 categories
         if(categoryCount != 4) {
             $('.categories-panel').append(addButton);
         }
     }
 }
 
+// Call this function when adding new category is canceled
 function removeNew() {
+
+    // Initialize the add-icon button
     const addButton = "<button id='category-add' class='add-icon font' title='Add Category' onclick='inputCategory()'><i class='icon fa-solid fa-plus'></i></button>";
+    // Get the length of the number of categories in categories-panel
     let categoryCount = $('.categories-panel').children('div').length;
+    // Get the number of buttons in the categories-panel
     let buttonCount = $('.categories-panel').children('button').length;
 
+    // Remove the temporary input field for new category
     $('.categories-panel .temp-input-box').remove();
 
+    // Append the add-icon button only when there are at most 3 categories and also only one add-icon button can be present in the categories-panel
     if((categoryCount == 2 || categoryCount == 3) && buttonCount != 1) {
         $('.categories-panel').append(addButton);
     }
 }
 
+// Call this when already existing category is to be removed
 $(document).on('click', '#removeCategory', function () {
+    // First get the ID of the category to be removed then pass it to the next function
     removeAdded($(this).parent().attr('id'));
 });
 function removeAdded(blockToRemove) {
+
+    // Initialize the add-icon button
     const addButton = "<button id='category-add' class='add-icon font' title='Add Category' onclick='inputCategory()'><i class='icon fa-solid fa-plus'></i></button>";
+    // Get the length of the number of categories in categories-panel and subtract 1 from it
     let categoryCount = $('.categories-panel').children('div').length - 1;
 
+
+    // Remove the category fron the categories-panel and remove the task lists associated with that category from the task-panel
     if(categoryCount+1 > 2) {
         $(`.categories-panel div#${blockToRemove}`).remove();
         $(`.task-panel div.${blockToRemove}`).remove();
     }
 
+    // Append the add-icon button if category count is 3
     if(categoryCount >= 2) {
         $('.categories-panel').append(addButton);
     }
-    // To check and remove extra add category icon
-    
+
+    // Remove the extra add-icon button if there are 2 categories. (So that it doesn't display 2 add-icon button)
     if (categoryCount == 2) {
         $('#category-add').remove();
     }
 
+    // If all the categories are deleted then make the first category selected by default
     if(categoryCount == 2) {
         makeNewActive($('.categories-panel div:first input[type=radio]').attr('id'));
     }
+    // Make the new added category selected
     else {
         makeNewActive($('.categories-panel div:last input[type=radio]').attr('id'));
     }
 }
 
+// Keyboard event
 $(document).on('keyup', '#categoryTitle', function (event) {
+    // If user presses enter, push the new category
     if(event.key == "Enter") {
         push();
     }
+    // If user presses escape, cancel the category addition procedure
     if(event.key == "Escape") {
         removeNew();
     }
 });
 
+// Call when new category is added to make it selected
 function makeNewActive(storeID) {
+    // First remove the active class from all the labels in the categories-panel
     $('.categories-panel div label').removeClass('active');
+    // Chnage the checked property of every input in the categories-panel to false
     $('.categories-panel div input[type="radio"]').prop('checked', false);
 
+    // Change the checked property of the new category in the categories-panel to true
     $(`.categories-panel div input[id='${storeID}'`).prop('checked', true);
+    // Lastly add the active class to the new category in the categories-panel
     $(`.categories-panel div label[for='${storeID}']`).addClass('active');
+
+    // Call this function to display contents of the selected category
     checkActive();
 }
+
+// Mouse event(For category selection)
 $(document).on('click', '.categories-panel div label', function(e) {
+
+    // Check if the clicked category has active class or not
     if($(`.categories-panel div label[for='${this.htmlFor}']`).hasClass('active') == false) {
+
+        // First remove the active class from all the labels in the categories-panel
         $('.categories-panel div label').removeClass('active');
+        // Chnage the checked property of every input in the categories-panel to false
         $('.categories-panel div input[type="radio"]').prop('checked', false);
 
+        // Change the checked property of the category that was clicked in the categories-panel to true
         $(`.categories-panel div input[id='${this.htmlFor}'`).prop('checked', true);
+        // Lastly add the active class to the category that was clicked in the categories-panel
         $(`.categories-panel div label[for='${this.htmlFor}']`).addClass('active');
+
+        // Call this function to display contents of the selected category
         checkActive();
     }
 });
-// ---------------------------------- Category section end
+
+// ---------------------------------- Category section end ----------------------------------
--- public/scripts/task.js
+++ public/scripts/task.js
@@ -1,35 +1,54 @@
-// ---------------------------------- Task section
+// ---------------------------------- Task section ----------------------------------
 
-// To check whichcategory is active
+// To check which category is active
 function checkActive() {
+
+    // Store the ID of the selected category
     let classID = $('.categories-panel div input[type="radio"]:checked').attr('id');
 
+    // If any div in the task-panel has class name resembling with the classID, then enter
     if($('.task-panel').children('div').hasClass(`${classID}`)) {
+
+        // First remove the hide class from all the div's in the task-panel
         $('.task-panel').children('div').addClass('hide');
+        // Now remove the hide class from the div that has class name resembling with the classID
         $(`.task-panel div.${classID}`).removeClass('hide');
     }
 }
 
-// To add new Task category
+// To add new Task section when a new category is added
 function addTaskCategory() {
+
+    // Get the length of the number of task sections in task-panel
     let categoryNumber = $('.task-panel').children('div').length;
+    // Get the length of the number of categories in categories-panel
     let categoryCount = $('.categories-panel').children('div').length;
+    // Get the name of the last category in categories-panel
     let categoryTitle = $('.categories-panel div:last input[type=radio]').attr('id');
 
+    // Create a new Task section with corresponding category title
     let newTaskCategory = 
         `<div class='category-${categoryNumber+1} ${categoryTitle}'>` + 
         '<h1>Kiklu</h1>' +
         '</div>';
     
+    // Append the new task section if category at kost 3 (Limit of 4 task sections)
     if(categoryNumber < 4) {
         $('.task-panel').append(newTaskCategory);
     }
 
+    // Get the length of the number of task sections in task-panel
     categoryNumber = $('.task-panel div').length;
+    // If number of categories is not euqal to number of task sections, remove the extra added task section
     if(categoryNumber != categoryCount) {
         $(`.task-panel div.category-${categoryNumber}`).remove();
     }
+
+    // Check which category is active
     checkActive();
 }
 
+// First function call to check which category is active
 checkActive();
+
+// ---------------------------------- Task section end ----------------------------------
